5bc6c26bd95204348d391faa2cb542617b06fd2d
Hugo Fortunato <hugo.fortunato@pixter.com.br>
2023-01-29 10:56:39 -0300

    Initial commit from Create Next App

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,14 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="logo" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Ilustration src="/img/hero-illustration.svg" alt="desenvolvedor" />
+  </S.Wrapper>
+)
+
+export default Main
